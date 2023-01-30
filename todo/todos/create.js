dfc9433fcd47c248195dcd1c7073eb813fa501e8
mehraj-hussain <mhussainceo@gmail.com>
2023-01-30 23:40:20 +0530

    some practise

--- todo/todos/create.js
+++ todo/todos/create.js
@@ -1,9 +1,47 @@
 import express from "express";
-import fs from 
+import fs from "fs/promises"
+import chalk from 'chalk';
+import readlineSync from 'readline-sync'
+import { randomStringGenerator, readFile,writeFile} from "../utils/index.js";
+
+async function createToDo() {
+    try {
+        console.clear();
+        console.log("==============================");
+        console.log("\tCreate a ToDo");
+        console.log("==============================");
+
+        let email = readlineSync.questionEMail('Enter Your Mail : ');
+        while(!email){
+            let email = readlineSync.questionEMail('Enter Your Mail : ');
+        }
+
+        let password = readlineSync.question('Enter Your Password : ');
+        // while(!password){
+        //     let password = readlineSync.questionInt('Enter Your Password : ');
+        // }
+
+     
+        let fileData = await fs.readFile('../data.json');
+        fileData = JSON.parse(fileData);
+
+        let passwordFound = fileData.find((ele) => ele.password == password);
+        let emailFound = fileData.find((ele) => ele.email == email);
+        if (!(emailFound || passwordFound)){
+            throw ('User Doesn\'t Exist');
+        }
+        let toDoName = readlineSync.question('Enter a ToDo Task : ');
+        while(!toDoName) {
+            let toDoName = readlineSync.question('Enter a ToDo Task : ');
+        }
 
 
 
 
-function signin(){
-    
+    }
+    catch(error){
+        console.log(error)
+    }
 }
+
+createToDo();
